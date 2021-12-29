1dc1504245de039b4e626f7ade72af7e3cb4fbe8
MinenkoV <minenko.od@gmail.com>
2021-12-29 22:20:51 +0200

    implemented console log with color

--- AppLogger/AppLogger.cs
+++ AppLogger/AppLogger.cs
@@ -4,5 +4,49 @@ namespace AppLogger
 {
     public class AppLogger
     {
+        public AppLogger(bool logToConsole, bool logToTelegram)
+        {
+            _logToConsole = logToConsole;
+            _logToTelegram = logToTelegram;
+        }
+
+        private readonly bool _logToConsole;
+        private readonly bool _logToTelegram;
+
+        public void LogMessage(string message, int messageLevel)
+        {
+            var logMessage = formMessage(message);
+        }
+
+        public void LogMessage(Exception ex, int messageLevel)
+        {
+            var message = ex.Message;
+            var logMessage = formMessage(message);
+            LogMessage(logMessage, 3);
+        }
+
+        private void printToConsole(string message, int messageLevel)
+        {
+            Console.ForegroundColor = messageLevel switch
+            {
+                1 => ConsoleColor.Green,
+                2 => ConsoleColor.Yellow,
+                3 => ConsoleColor.Red,
+                _ => ConsoleColor.Gray
+            };
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private void sendToTelegram(string message)
+        {
+            
+        }
+
+        private string formMessage(string message) =>
+            $"{DateTime.Now}:\n" +
+            $"PC: {Environment.MachineName}, User: {Environment.UserName}\n" +
+            $"Message from method\"{new System.Diagnostics.StackTrace().GetFrame(2)?.GetMethod()?.Name}\":\n" +
+            $"{message}";
     }
 }
--- AppLogger/Helpers/TelegramHelper.cs
+++ AppLogger/Helpers/TelegramHelper.cs
@@ -1,7 +1,48 @@
-﻿namespace AppLogger.Helpers
+﻿using Telegram.Bot;
+
+namespace AppLogger.Helpers
 {
     public class TelegramHelper
     {
-        
+        #region Init
+
+        private readonly ITelegramBotClient _telegramBotClient;
+        private readonly int _chatId;
+
+        public TelegramHelper(int chatId, ITelegramBotClient telegramBotClient)
+        {
+            _chatId = chatId;
+            _telegramBotClient = telegramBotClient;
+        }
+
+        #endregion
+
+        #region PublicMethods
+
+        /// <summary>
+        /// Sends text message to chat
+        /// </summary>
+        /// <param name="logMessage">Message to be send</param>
+        /// <param name="disableNotification">Disable notification to receiver</param>
+        public void SendMessage(string logMessage, bool disableNotification)
+        {
+            _telegramBotClient.SendChatActionAsync(_chatId, Telegram.Bot.Types.Enums.ChatAction.Typing).Wait();
+            _telegramBotClient.SendTextMessageAsync(_chatId, logMessage, disableNotification: disableNotification)
+                .Wait();
+        }
+
+        /// <summary>
+        /// Sends text message to chat
+        /// </summary>
+        /// <param name="logMessage">Message to be send</param>
+        /// <param name="disableNotification">Disable notification to receiver</param>
+        public async void SendMessageAsync(string logMessage, bool disableNotification)
+        {
+            await _telegramBotClient.SendChatActionAsync(_chatId, Telegram.Bot.Types.Enums.ChatAction.Typing);
+            await _telegramBotClient.SendTextMessageAsync(_chatId, logMessage,
+                disableNotification: disableNotification);
+        }
+
+        #endregion
     }
 }
